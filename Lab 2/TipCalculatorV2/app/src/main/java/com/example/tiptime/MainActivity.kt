b4d80a91c0f3b3563499c1328bc722ae58b13101
Yaroslav Zahorodnyi <zahorodnyi.yaroslav.work@gmail.com>
2023-11-25 11:50:43 +0100

    TipCalculatorV2 unit and UI tests

--- Lab 2/TipCalculatorV2/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Lab 2/TipCalculatorV2/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -182,7 +182,7 @@ fun RoundTheTipRow(
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+public fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
     if (roundUp) {
         tip = kotlin.math.ceil(tip)
