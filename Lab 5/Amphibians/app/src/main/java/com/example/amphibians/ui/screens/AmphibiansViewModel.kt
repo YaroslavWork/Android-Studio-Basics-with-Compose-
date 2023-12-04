af59f8a42a6876408ea6edf56fa714a40345b483
Yaroslav Zahorodnyi <zahorodnyi.yaroslav.work@gmail.com>
2023-12-04 16:54:39 +0100

    Amphibians create architecture, viewModel, DI, Application

---
+++ Lab 5/Amphibians/app/src/main/java/com/example/amphibians/ui/screens/AmphibiansViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.amphibians.ui.screens
+
+import android.net.http.HttpException
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.amphibians.AmphibiansApplication
+import com.example.amphibians.data.AmphibiansRepository
+import com.example.amphibians.model.Amphibian
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class AmphibiansUiState {
+    object Loading: AmphibiansUiState()
+    data class Success(val amphibians: List<Amphibian>): AmphibiansUiState()
+    data class Error(val message: String): AmphibiansUiState()
+}
+class AmphibiansViewModel(
+    private val amphibiansRepository: AmphibiansRepository
+) : ViewModel() {
+    var amphibiansUiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
+        private set
+
+    init {
+        getAmphibians()
+    }
+
+    fun getAmphibians() {
+        viewModelScope.launch {
+            amphibiansUiState = AmphibiansUiState.Loading
+            amphibiansUiState = try {
+                AmphibiansUiState.Success(amphibiansRepository.getAmphibians())
+            } catch (e: IOException) {
+                AmphibiansUiState.Error(e.message ?: "An unknown error occurred")
+            }
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AmphibiansApplication)
+                val amphibiansRepository = application.container.amphibiansRepository
+                AmphibiansViewModel(amphibiansRepository = amphibiansRepository)
+            }
+        }
+    }
+}
